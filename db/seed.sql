7c010b33f09297e4257bc09c91540c275c779d3d
Joseph <JSPenland97@gmai.com>
2021-03-03 18:42:19 -0500

    fixes

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,4 @@
-INSERT INTO burgers (name)
-VALUES 
-("Plymouth Roquefort Burger"),
-("We're Here, We're Gruyere, Get Used to it Burger"),
-("Put Me in Poached Burger"),
-("Poutine on the Ritz Burger");
-
-INSERT INTO burgers (name, devoured)
-VALUES 
-("I Don't Bay Leaf in Magic Burger", true),
-("She's a Super Leek Burger", true),
-("Running Out of Thyme Burger", true);
+  
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('WidowMaker');
+INSERT INTO burgers (name) VALUES ('CardioDestroyer');
